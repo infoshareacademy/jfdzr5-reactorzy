494084bbc4b05d299ed76696c91c3a212bb6b513
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2022-01-09 19:34:06 +0100

    add MUI UI Component and styled Component

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,24 @@
+export const Dashboard = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="about-app">
+          <h1>Koffee</h1>
+          <p>
+            powered by <strong>Reactorzy</strong>
+          </p>
+          <p>
+            <span>6420</span> napisanych postów
+          </p>
+          <p>
+            <span>526980</span> napisanych komentarzy
+          </p>
+          <p>
+            <span>11</span> zarejestrowanych użytkowników
+          </p>
+        </div>
+        <div className="registration-panel"></div>
+      </div>
+    </>
+  );
+};
