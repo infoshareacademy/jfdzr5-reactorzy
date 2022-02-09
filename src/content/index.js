02dd21b86c543ade40192f2b2312b27a4764c4ec
espezaliate <mkoziol54@gmail.com>
2022-02-09 17:17:50 +0100

    moved files + created posts outline. Error with use effect if user authed

--- src/content/index.js
+++ src/content/index.js
@@ -1,12 +1,14 @@
-import SignInSide from "./dashboard";
+import SignInSide from "./landing/signin/dashboard";
 import { Routes, Route } from "react-router-dom";
-import SignUp from "./dashboard/signup";
+import SignUp from "./landing/signin/signup";
+import MainView from "./landing/home";
+import { useUserContext } from "../services/user-context";
 
 export const Content = () => {
+  const { user } = useUserContext();
   return (
     <Routes>
-      <Route path="/" element={<SignInSide />} />
-      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/" element={user ? <MainView /> : <SignInSide />} />
       <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   );
---
+++ src/content/landing/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TextPost from "../../posts/TextPost";
+
+const MainView = () => {
+  return (
+    <div>
+      <TextPost />
+    </div>
+  );
+};
+
+export default MainView;
