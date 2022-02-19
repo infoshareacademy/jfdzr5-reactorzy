3074d30494397c058f9f29d88d42d75d6d0553db
Kalbarczyk Kamil <kkalbarczyk@networks.pl>
2022-02-19 22:38:44 +0100

    add app bar from Material UI and app logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Content } from "./content";
 import { UserContextProvider } from "./services/user-context";
+import { Navigation } from "./navigation";
 
 function App() {
   return (
     <>
       <UserContextProvider>
+        <Navigation />
         <Content />
       </UserContextProvider>
     </>
