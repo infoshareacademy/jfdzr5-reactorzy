858ac9aa3258a996be74da91cb8896e8d698833b
espezaliate <mkoziol54@gmail.com>
2022-02-22 17:26:40 +0100

    fixed loading bug

--- src/content/landing/index.js
+++ src/content/landing/index.js
@@ -1,16 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useUserContext } from "../../services/user-context";
 import MainView from "./home";
 import SignInSide from "./signin/dashboard";
 import Loading from "../../common/loading";
 
 export const Landing = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const { user } = useUserContext();
-
-  useEffect(() => {
-    setIsLoading(false);
-  }, []);
+  const { user, isLoading } = useUserContext();
 
   return isLoading ? <Loading /> : user ? <MainView /> : <SignInSide />;
 };
